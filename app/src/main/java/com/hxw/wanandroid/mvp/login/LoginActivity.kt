19d11e82cb48d50e5e613abdc00e58335cd522ed
huxiaowei <huxiaowei008@126.com>
2018-06-12 10:54:20 +0800

    增加转场动画和过渡动画

--- app/src/main/java/com/hxw/wanandroid/mvp/login/LoginActivity.kt
+++ app/src/main/java/com/hxw/wanandroid/mvp/login/LoginActivity.kt
@@ -1,12 +1,15 @@
 package com.hxw.wanandroid.mvp.login
 
+import android.app.ActivityOptions
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import com.hxw.core.base.AbstractActivity
 import com.hxw.wanandroid.R
 import com.hxw.wanandroid.mvp.register.RegisterActivity
 import kotlinx.android.synthetic.main.activity_login.*
 import timber.log.Timber
+
 
 /**
  * @author hxw on 2018/6/2.
@@ -19,7 +22,22 @@ class LoginActivity : AbstractActivity() {
 
     override fun init(savedInstanceState: Bundle?) {
 
-        fa_btn.setOnClickListener { startActivity(Intent(this, RegisterActivity::class.java)) }
+        fa_btn.setOnClickListener {
+            /**
+             * 过渡动画需要在5.0版本以上
+             * 对于transitionName,可以在xml中设置,注意版本在5.0以上
+             * 或者定义成常量,在onCreate()中通过
+             * ViewCompat.setTransitionName(mHeaderImageView, VIEW_NAME_HEADER_IMAGE);
+             * 方法设置(或使用View.setTransitionName()方法5.0版本)
+             */
+            val options = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                ActivityOptions
+                        .makeSceneTransitionAnimation(this, fa_btn, fa_btn.transitionName)
+            } else {
+                null
+            }
+            startActivity(Intent(this, RegisterActivity::class.java), options?.toBundle())
+        }
 
         btn_login.setOnClickListener {
             Timber.i("登陆")
--- app/src/main/java/com/hxw/wanandroid/mvp/register/RegisterActivity.kt
+++ app/src/main/java/com/hxw/wanandroid/mvp/register/RegisterActivity.kt
@@ -1,6 +1,15 @@
 package com.hxw.wanandroid.mvp.register
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.os.Build
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.transition.Transition
+import android.transition.TransitionInflater
+import android.view.View
+import android.view.ViewAnimationUtils
+import android.view.animation.AccelerateInterpolator
 import com.hxw.core.base.AbstractActivity
 import com.hxw.wanandroid.R
 import kotlinx.android.synthetic.main.activity_register.*
@@ -18,6 +27,106 @@ class RegisterActivity : AbstractActivity() {
 
     override fun init(savedInstanceState: Bundle?) {
 
-        fa_btn.setOnClickListener { finish() }
+        fa_btn.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                animateRevealClose()
+            } else {
+                finishActivity()
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            showEnterAnimation()
+        }
+    }
+
+    /**
+     * 若要在完成第二个活动时反转场景转换动画,请调用Activity.finishAfterTransition()方法
+     * 而不是Activity.finish(),注意版本5.0
+     */
+    private fun finishActivity() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            finishAfterTransition()
+        } else {
+            finish()
+        }
+    }
+
+    /**
+     * 制作展示进入的动画
+     */
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun showEnterAnimation() {
+
+
+        val transition = TransitionInflater.from(this).inflateTransition(R.transition.fab_transition)
+
+        window.sharedElementEnterTransition = transition
+        transition.addListener(object : Transition.TransitionListener {
+            override fun onTransitionStart(transition: Transition) {
+                card_register.visibility = View.GONE
+            }
+
+
+            override fun onTransitionEnd(transition: Transition) {
+                transition.removeListener(this)
+                animateRevealShow()
+            }
+
+            override fun onTransitionCancel(transition: Transition) {
+
+            }
+
+            override fun onTransitionPause(transition: Transition) {
+
+            }
+
+            override fun onTransitionResume(transition: Transition) {
+
+            }
+
+
+        })
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun animateRevealShow() {
+        val mAnimator = ViewAnimationUtils.createCircularReveal(card_register, card_register.width / 2,
+                0, (fa_btn.width / 2).toFloat(), card_register.height.toFloat())
+        mAnimator.duration = 500
+        mAnimator.interpolator = AccelerateInterpolator()
+        mAnimator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                super.onAnimationStart(animation)
+                card_register.visibility = View.VISIBLE
+            }
+        })
+        mAnimator.start()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun animateRevealClose() {
+        val mAnimator = ViewAnimationUtils.createCircularReveal(card_register, card_register.width / 2,
+                0, card_register.height.toFloat(), (fa_btn.width / 2).toFloat())
+        mAnimator.duration = 500
+        mAnimator.interpolator = AccelerateInterpolator()
+        mAnimator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                super.onAnimationEnd(animation)
+                card_register.visibility = View.INVISIBLE
+                finishActivity()
+            }
+        })
+        mAnimator.start()
+    }
+
+    override fun onBackPressed() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            animateRevealClose()
+        } else {
+            super.onBackPressed()
+        }
     }
 }
