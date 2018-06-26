604f6df496be1919b347e343552912c3d30ea4a0
huxiaowei <huxiaowei008@126.com>
2018-06-26 15:36:03 +0800

    简略搭建main界面

--- app/src/main/java/com/hxw/wanandroid/mvp/MainActivity.kt
+++ app/src/main/java/com/hxw/wanandroid/mvp/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.hxw.wanandroid.mvp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.hxw.core.base.AbstractActivity
+import com.hxw.core.utils.StatusBarUtils
 import com.hxw.wanandroid.R
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : AbstractActivity() {
+    override fun getLayoutId(): Int {
+        return R.layout.activity_main
     }
+
+    override fun init(savedInstanceState: Bundle?) {
+        StatusBarUtils.noStatusBar(this)
+
+        nav_view.setNavigationItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.nav_camera->{}
+                else -> {
+
+                }
+            }
+
+
+            return@setNavigationItemSelectedListener true
+        }
+    }
+
 }
